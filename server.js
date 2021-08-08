dd7ca0e7078f881015fbc662ca083be8b3083af2
Esther <tetececilia231@gmail.com>
2021-08-08 10:27:28 -0300

    Ajuste header request

--- server.js
+++ server.js
@@ -1,38 +1,29 @@
 const express = require("express");
-// const cors = require("cors");
-const handlebars = require("express-handlebars");
-const bodyParser = require("body-parser");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
 const rota = require("./routes");
-var assert = require("assert");
 
-// var corsOptions = {
-//   origin: "http://localhost:8081",
-// };
+var corsOptions = {
+  origin: "http://localhost:8081",
+};
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 app.use(express.json());
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.engine("handlebars", handlebars({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-mongoose.connect(
-  "mongodb+srv://root:123@cluster0.wnxni.mongodb.net/api-posto",
-  {
+mongoose
+  .connect("mongodb+srv://root:123@cluster0.wnxni.mongodb.net/api-posto", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
     useCreateIndex: true,
-  },
-  function (err, db) {
-    assert.equal(null, err);
-    console.log("Connected correctly to server");
-  }
-);
+  })
+  .then(() => {
+    console.log("Conexão no banco de dados realizada com sucesso");
+  })
+  .catch(() => {
+    console.log("Conexão com banco de dados não realizada");
+  });
 
 app.use("/", rota);
 
