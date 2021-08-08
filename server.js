ce92170a1b9b88a3237a75e2e95c320e7b042608
Esther <tetececilia231@gmail.com>
2021-08-08 18:24:41 -0300

    Ajuste procfile

--- server.js
+++ server.js
@@ -4,37 +4,31 @@ const mongoose = require("mongoose");
 const app = express();
 const rota = require("./routes");
 
-async function main() {
-  let corsOptions = {
-    origin: "http://localhost:8081",
-  };
+var corsOptions = {
+  origin: "http://localhost:8081",
+};
 
-  let resBanco = "";
+app.use(cors(corsOptions));
+app.use(express.json());
 
-  app.use(cors(corsOptions));
-  app.use(express.json());
-
-  await mongoose
-    .connect("mongodb+srv://root:123@cluster0.wnxni.mongodb.net/api-posto", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    })
-    .then(() => {
-      resBanco = "Conexão no banco de dados realizada com sucesso";
-    })
-    .catch(() => {
-      resBanco = "Conexão com banco de dados não realizada";
-    });
-
-  app.use("/", rota);
-
-  const PORT = process.env.PORT || 8080;
-
-  app.listen(PORT, () => {
-    console.log(resBanco);
+mongoose
+  .connect("mongodb+srv://root:123@cluster0.wnxni.mongodb.net/api-posto", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("Conexão no banco de dados realizada com sucesso");
+  })
+  .catch(() => {
+    console.log("Conexão com banco de dados não realizada");
   });
-}
 
-main();
+app.use("/", rota);
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log("servidor rodando ");
+});
