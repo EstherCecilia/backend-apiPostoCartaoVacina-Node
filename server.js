151bd1aeb75d9c9c2b0983a91d44018c154b913a
Esther <tetececilia231@gmail.com>
2021-08-08 10:21:57 -0300

    Ajuste header request

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
 const rota = require("./routes");
+var assert = require("assert");
 
 // var corsOptions = {
 //   origin: "http://localhost:8081",
@@ -19,19 +20,19 @@ app.use(bodyParser.json());
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-mongoose
-  .connect("mongodb+srv://root:123@cluster0.wnxni.mongodb.net/api-posto", {
+mongoose.connect(
+  "mongodb+srv://root:123@cluster0.wnxni.mongodb.net/api-posto",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
     useCreateIndex: true,
-  })
-  .then(() => {
-    console.log("Conexão no banco de dados realizada com sucesso");
-  })
-  .catch(() => {
-    console.log("Conexão com banco de dados não realizada");
-  });
+  },
+  function (err, db) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server");
+  }
+);
 
 app.use("/", rota);
 
